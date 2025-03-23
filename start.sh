82e10b4c4335ad928cdb6bd508da77d431e2efb3
Kyle Kirkby <kylekirkbyy@gmail.com>
2025-03-23 14:47:00 +0000

    Remove PostgreSQL readiness check from startup script
    - Eliminate the waiting mechanism for PostgreSQL to be ready in the start.sh script
    - Streamline the script to focus on running database updates without the readiness check

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Wait for PostgreSQL to be ready
-echo "Waiting for PostgreSQL to be ready..."
-while ! nc -z postgres 5432; do
-  sleep 1
-done
-echo "PostgreSQL is ready!"
-
 # Run database updates
 echo "Running database updates..."
 cd lib/ast
